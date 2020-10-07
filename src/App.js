1d6e05bedaaf97ffb2f54b41150f8fb3bd355b2d
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-07 13:49:27 -0600

    performed axios.get

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
+// import the axios lib from node_modules
+import axios from 'axios'
+
+// import the contants from constants/index.js
+import { BASE_URL, API_KEY } from './constants/index'
+
+// import Details from './Details'
+
 function App() {
+
+  const [photos, setPhotos] = useState([])
+
+  useEffect(() => {
+    const fetchPhotos = () => {
+      axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+        .then(res => {
+          console.log(res.data)
+        })
+        .catch(err => {
+          debugger
+        })
+    }
+    fetchPhotos()
+  }, [])
+
   return (
     <div className="App">
       <p>
