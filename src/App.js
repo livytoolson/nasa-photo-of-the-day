521b607a0b36bf31270718ac0366dd6ff009cbb9
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-07 15:09:08 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <div className="App">
-    <h1>NASA</h1>
+    <h1>NASA PHOTO OF THE DAY</h1>
       <p>
         {/* Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>! */}
